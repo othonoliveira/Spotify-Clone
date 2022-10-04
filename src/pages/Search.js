534c0280dd650c6e7857b44941007ebbb023c43b
Othon Oliveira <79730183+othonoliveira@users.noreply.github.com>
2022-10-04 13:31:46 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artist: '',
+    buttonState: true,
+  };
+
+  handleArtist = (event) => {
+    this.setState({ artist: event.target.value }, this.toggleButton);
+  };
+
+  toggleButton = () => {
+    const { artist } = this.state;
+    const MIN_LENGTH = 2;
+    if (artist.length >= MIN_LENGTH) {
+      this.setState({ buttonState: false });
+    } else {
+      this.setState({ buttonState: true });
+    }
+  };
+
   render() {
+    const { buttonState } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-input">
+            <input
+              type="text"
+              name="search-input"
+              data-testid="search-artist-input"
+              onChange={ this.handleArtist }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ buttonState }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
